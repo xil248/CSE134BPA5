00d775d78b6a9d2ac970a600e743043ff697310e
Xinghang Li <xil248@ucsd.edu>
2018-02-23 12:36:33 -0800

    ignore

--- src/components/client/Restaurants.js
+++ src/components/client/Restaurants.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router';
 import '../../styles/Restaurants.css'
-import '../../images/'
+// import '../../images/'
 
 class HomePage extends React.Component {
   
