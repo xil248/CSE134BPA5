42d1ac830bbc1f676a74c25b082510797ac531f7
Xinghang Li <xil248@ucsd.edu>
2018-02-23 12:18:44 -0800

    all

--- src/components/client/Restaurants.js
+++ src/components/client/Restaurants.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import {Link} from 'react-router';
+import '../../styles/Restaurants.css'
+import '../../images/'
 
 class HomePage extends React.Component {
+  
   render() {
     return (
       <div>
         <h1>Restaurants</h1>
+        <div className="floating-box">
+          <img src='../../images/McDonald.png'/>
+          <h2> McDonald's </h2>
+        </div>
+        {/* <img src="./" alt="McDonald" style="width:250px;height:200px"/> */}
+        {/* <div class="floating-box">
+            <img src="./images/McDonald.png" alt="McDonald" style="width:250px;height:200px"/>
+            <h2> McDonald's </h2>
+            <h3> American </h3>
+            <img src="./images/Rate3.png" alt="Rate3" style="width:60px;height:15px"/>
+        </div> */}
       </div>
     );
   }
